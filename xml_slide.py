31d767f6887fdc7036de43330e15a3981addf21e
mukeshchugani10 <mukeshchugani10@gmail.com>
2019-06-12 16:48:47 +0530

    rectified filenames in xml

--- xml_slide.py
+++ xml_slide.py
@@ -6,6 +6,8 @@ for filename in os.listdir('data/images/train/'):
 		tree = ET.parse('data/images/train/'+filename)
 		root = tree.getroot()
 		for elem in root:
+			if elem.tag == "filename":
+				elem.text = filename
 			if (elem.tag=="object" and "slide" in [child.text for child in elem]) or elem.tag != "object":
 				pass
 			else:
