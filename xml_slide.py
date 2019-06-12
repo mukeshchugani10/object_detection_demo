c9822f13262111ca5629adda78856ca21329933e
mukeshchugani10 <mukeshchugani10@gmail.com>
2019-06-12 17:24:05 +0530

    Bug fixed-xml to jpg

--- xml_slide.py
+++ xml_slide.py
@@ -7,9 +7,11 @@ for filename in os.listdir('data/images/train/'):
 		root = tree.getroot()
 		for elem in root:
 			if elem.tag == "filename":
-				elem.text = filename
+				elem.text = filename[:-4]+'.jpg'
 			if elem.tag == "folder":
 				elem.text = 'train'
+			if elem.tag == "path":
+				elem.text = os.path.abspath('data/images/train/'+filename[:-4]+'.jpg')
 			if (elem.tag=="object" and "slide" in [child.text for child in elem]) or elem.tag != "object":
 				pass
 			else:
