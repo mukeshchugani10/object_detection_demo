fcd95e3ff7221d48aa797e359df47ec861410353
mukeshchugani10 <mukeshchugani10@gmail.com>
2019-06-13 21:58:17 +0530

    Added exclusive eq imgs to test

--- eq_selector.py
+++ eq_selector.py
@@ -6,12 +6,17 @@ import xml.etree.ElementTree as ET
 
 for filename in os.listdir('test/'):
 	if filename.endswith('.xml'):
+		f = 0
 		tree = ET.parse('test/'+filename)
 		root = tree.getroot()
 		for elem in root:
-			if (elem.tag=="object" and "eq" in [child.text for child in elem]) or elem.tag != "object":
-				pass
+			if (elem.tag=="object"):
+				if("eq" in [child.text for child in elem]):
+					f = 1
+				else:
+					root.remove(elem)
 			else:
-				root.remove(elem)
-		tree.write('data/images/test/'+filename)
-		os.rename('test/'+filename[:-3]+'jpg','data/images/test/'+filename[:-3]+'jpg')
+				pass
+		if(f==1):
+			tree.write('data/images/test/'+filename)
+			os.rename('test/'+filename[:-3]+'jpg','data/images/test/'+filename[:-3]+'jpg')
